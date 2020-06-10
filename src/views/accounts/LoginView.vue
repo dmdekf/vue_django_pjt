96bbcc1c66352f792067e8cc3fab8d87969ccb61
dmdekf <dmdekf@gmail.com>
2020-06-10 21:25:01 +0900

    2

--- src/views/accounts/LoginView.vue
+++ src/views/accounts/LoginView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="container">
     <h1>로그인</h1>
-    <form>
-      <div class="form-group">
-        <label for="username">아이디</label>
-        <input class="form-control" v-model="loginData.username" type="text" id="username" />
-        <small class="form-text text-muted">아이디를 작성해주세요</small>
-      </div>
-      <div class="form-group">
-        <label for="password">비밀번호</label>
-        <input class="form-control" v-model="loginData.password" type="password" id="password" />
-        <small class="form-text text-muted">비밀번호를 작성해주세요</small>
-      </div>
-    </form>
+    <div class="form-group">
+      <label for="username">아이디</label>
+      <input class="form-control" v-model="loginData.username" type="text" id="username" />
+      <small class="form-text text-muted">아이디를 작성해주세요</small>
+    </div>
+    <div class="form-group">
+      <label for="password">비밀번호</label>
+      <input class="form-control" v-model="loginData.password" type="password" id="password" />
+      <small class="form-text text-muted">비밀번호를 작성해주세요</small>
+    </div>
     <div>
       <button class="btn btn-primary" @click="login">확인</button>
     </div>
--- src/views/accounts/SignupView.vue
+++ src/views/accounts/SignupView.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="container">
     <h1>회원가입</h1>
-    <form>
-      <div class="form-group">
-        <label for="username">아이디</label>
-        <input class="form-control" v-model="signupData.username" type="text" id="username" />
-        <small class="form-text text-muted">아이디를 작성해주세요</small>
-      </div>
-      <div>
-        <label for="password1">비밀번호</label>
-        <input class="form-control" v-model="signupData.password1" type="password" id="password1" />
-        <small class="form-text text-muted">비밀번호를 작성해주세요</small>
-      </div>
-      <div>
-        <label for="password2">비밀번호 확인</label>
-        <input class="form-control" v-model="signupData.password2" type="password" id="password2" />
-        <small class="form-text text-muted">비밀번호를 확인해주세요</small>
-      </div>
-    </form>
+    <div class="form-group">
+      <label for="username">아이디</label>
+      <input class="form-control" v-model="signupData.username" type="text" id="username" />
+      <small class="form-text text-muted">아이디를 작성해주세요</small>
+    </div>
+    <div>
+      <label for="password1">비밀번호</label>
+      <input class="form-control" v-model="signupData.password1" type="password" id="password1" />
+      <small class="form-text text-muted">비밀번호를 작성해주세요</small>
+    </div>
+    <div>
+      <label for="password2">비밀번호 확인</label>
+      <input class="form-control" v-model="signupData.password2" type="password" id="password2" />
+      <small class="form-text text-muted">비밀번호를 확인해주세요</small>
+    </div>
     <div>
       <button class="btn btn-primary" @click="signup">확인</button>
     </div>
